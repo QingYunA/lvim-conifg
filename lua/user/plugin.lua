29cab23e565b1eadec443255ee49d7eeda6f8d56
Serein <serein7z@163.com>
2023-06-26 19:26:11 +0800

    update keybing settings

--- lua/user/plugin.lua
+++ lua/user/plugin.lua
@@ -87,6 +87,21 @@ M.config = function()
       "sindrets/diffview.nvim",
       event = "BufRead",
     },
+    -- clever f --
+    { "rhysd/clever-f.vim" },
+    -- github copilot --
+    {
+      "zbirenbaum/copilot-cmp",
+      event = "InsertEnter",
+      dependencies = { "zbirenbaum/copilot.lua" },
+      config = function()
+        vim.defer_fn(function()
+          require("copilot").setup() -- https://github.com/zbirenbaum/copilot.lua/blob/master/README.md#setup-and-configuration
+          require("copilot_cmp").setup() -- https://github.com/zbirenbaum/copilot-cmp/blob/master/README.md#configuration
+        end, 100)
+      end,
+    }
+
   }
 end
 return M
